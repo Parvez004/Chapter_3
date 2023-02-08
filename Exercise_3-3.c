8d905daaf942d37f34b9d297dc293d25052ce86c
Parvez004 <riadprvzz@gmail.com>
2023-02-08 15:24:00 +0600

    typedef

--- Exercise_3-3.c
+++ Exercise_3-3.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    
+   int x, y;
+   short int z;
+   x=322123;
+   y=544365;
+   z=x+y;
+   printf("x=%d\ny=%d\nz=%d", x, y, z);
+   return 0;
+   /*Result is 14520, Which is incorrect.*/ 
 }
---
+++ Exercise_3-5_typedef.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+typedef int INTEGER;
+
+int main()
+{
+    INTEGER num;
+    num=404;
+    printf("%d", num);
+    return 0;
+}
